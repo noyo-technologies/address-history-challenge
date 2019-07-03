8d89cf013d5e52b5a8b17cdaa3867b541f582a6b
Brian Timm <brian@noyotechnologies.com>
2019-07-03 13:27:07 -0600

    Allow migrate script to wait for DB container to start

--- migrate-up.js
+++ migrate-up.js
@@ -6,8 +6,10 @@ const path = require('path')
 const connectionString = `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_DATABASE}`
 const cmd = `node_modules/.bin/sequelize db:migrate --url="${connectionString}" --migrations-path="${path.join(__dirname, 'migrations')}"`
 
-exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    throw new Error(error)
-  }
-})
+setTimeout(() => {
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      throw new Error(error)
+    }
+  })
+}, 7500)
